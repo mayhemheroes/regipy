14d0c4d64ab37eb92c50fd0839f4679ed18c6057
McKenna <dallmeyerm@att.net>
2023-05-27 23:48:49 -0400

    Adjusted count for error

--- mayhem/fuzz_reg.py
+++ mayhem/fuzz_reg.py
@@ -22,7 +22,7 @@ def TestOneInput(data):
     except (ConstError, UnicodeDecodeError, StreamError):
         return -1
     except (struct.error, StopIteration) as e:
-        if ctr > 10000:
+        if ctr > 1000:
             raise e
         return -1
 
